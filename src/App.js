6942086e7dd56099770eb9db6b0b08f360dbfde6
Pratham shah <shahpratham130@gmail.com>
2023-01-29 17:47:20 +0545

    some changes in App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App(){
         <div className="container mb-3">
   <label htmlFor="exampleFormControlTextarea1" className="form-label">Enter text to Convert Into UpperCase</label>
   <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={currTxt}onChange={change}></textarea>
-  <button className="btn btn-primary my-3"onClick={convert}>Convert</button>
+  <button className="btn btn-primary my-3"onClick={convert}>Covert to UpperCase</button>
 </div>
 <div className="container">
   <h2 className="heading">Text Summary</h2>
